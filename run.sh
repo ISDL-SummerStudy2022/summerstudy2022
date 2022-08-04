0807c579b4a2fb52a2cc016dd16320c84494be6b
antic9 <39718108+antic9@users.noreply.github.com>
2022-08-04 20:44:17 +0900

    merge init projects

--- run.sh
+++ run.sh
@@ -9,4 +9,4 @@ fi
 
 # docker
 docker-compose build
-docker compose run --rm react-app sh -c "yarn global add create-react-app && create-react-app app --template typescript"
+docker-compose run --rm react-app sh -c "yarn global add create-react-app && create-react-app app --template typescript"
