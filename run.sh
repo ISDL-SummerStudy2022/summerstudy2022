f636aaf962bf3c60bc5296770955780e104b251c
ryo-jpg <73480314+ryo-jpg@users.noreply.github.com>
2022-08-04 19:24:11 +0900

    edit run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
 
+# ディレクトリがある確認
+DIR="app"
+if [ ! -d $DIR ];then
+  rm -rf $DIR
+fi
+
+# docker
 docker-compose build
 docker compose run --rm react-app sh -c "yarn global add create-react-app && create-react-app app --template typescript"
