9d72b0748b3a2a1853064261b1e90ec0ad971414
antic9 <39718108+antic9@users.noreply.github.com>
2022-08-06 18:14:50 +0900

    implement login func

--- front/app/src/index.tsx
+++ front/app/src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './assets/css/styles.scss'; //基本となるstyles.scssを読み込み
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+   <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
