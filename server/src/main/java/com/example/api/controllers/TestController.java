f26bc99b4847a0ab71ff7c1cd5a009fcc83c1586
Knpand <f.kntr514@gmail.com>
2022-08-07 05:47:39 +0900

    creating mockserver

--- server/src/main/java/com/example/api/controllers/TestController.java
+++ server/src/main/java/com/example/api/controllers/TestController.java
@@ -1,8 +1,8 @@
 package com.example.api.controllers;
 
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.MediaType;
+import java.util.List;
 
 @RestController
 public class TestController {
@@ -11,17 +11,45 @@ public class TestController {
       return "ok from test.";
     }
 
-    @RequestMapping(path = "/test/json", method = RequestMethod.GET)
-    public Example testJson() {
-      var examplePOJO = new Example();
-      examplePOJO.value1 = "foo";
-      examplePOJO.value2 = "bar";
-      return examplePOJO;
+    @RequestMapping(path = "/test/json", method = RequestMethod.GET,consumes = MediaType.APPLICATION_JSON_VALUE)
+      
+      @ResponseBody
+
+      public List<Testjson> responsejson(
+          @RequestBody List<Testjson> testjson) {
+            System.out.println("testing");
+            return testjson;
+      }
+
+
+      @RequestMapping(path = "/test/json2", method = RequestMethod.GET,consumes = MediaType.APPLICATION_JSON_VALUE)
+      @ResponseBody
+      public Testjson responsejson2(
+          @RequestBody Testjson testjson) {
+            System.out.println("testing");
+            return testjson;
+      }
+
+      public static class Testjson {
+        private String value1;
+        private String value2;
+
+        public String getValue1(){
+          return value1;
+        }
+        public String getValue2(){
+          return value2;
+        }
+        public void setValue1(String value1){
+          this.value1=value1;
+        }
+        public void setValue2(String value2){
+          this.value2=value2;
+        }
+
+      }
+  
     }
 
-    public class Example {
-  		public String value1;
-	  	public String value2;
-	  }
-}
+
 
