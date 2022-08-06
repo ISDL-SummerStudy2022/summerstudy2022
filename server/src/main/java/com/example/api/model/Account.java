f671d27a01214b4a4715002279245d7d41bbc2e6
Knpand <f.kntr514@gmail.com>
2022-08-06 19:16:43 +0900

    creating auth #12

---
+++ server/src/main/java/com/example/api/model/Account.java
@@ -0,0 +1,30 @@
+package com.example.api.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+@Access(AccessType.FIELD)
+public class Account {
+
+    @Id
+    private String userId;
+
+    private String password;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
